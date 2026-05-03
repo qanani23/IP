b32a4558e3c6065c622d909f490cde8a7820b591
qanani23 <keneniarfaso@gmail.com>
2026-05-03 05:43:19 -0700

    Configure project for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ✅ FIXED for Netlify
   plugins: [react()],
   test: {
     environment: 'jsdom',
